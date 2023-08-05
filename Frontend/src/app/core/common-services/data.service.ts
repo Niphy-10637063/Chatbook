4ca619d822c6ca42730203e6a0300df67469dcc2
Niphy-10637063 <128737044+Niphy-10637063@users.noreply.github.com>
2023-08-05 22:19:44 +0100

    unwanted codes removed

--- Frontend/src/app/core/common-services/data.service.ts
+++ Frontend/src/app/core/common-services/data.service.ts
@@ -52,56 +52,5 @@ export class DataService {
     );
   }
 
-  getCategoriesWithPdtCount(): Observable<any> {
-    return this.httpService.get(
-      this.baseUrl + Strings.API_ENDPOINT_GET_CATEGORIES_WITH_PRODUCT_COUNT
-    );
-  }
-
-  deleteCategory(id: number): Observable<any> {
-    return this.httpService.delete(
-      this.baseUrl + Strings.API_ENDPOINT_DELETE_CATEGORY + id
-    );
-  }
-  saveProduct(body: any): Observable<any> {
-    return this.httpService.post(
-      this.baseUrl + Strings.API_ENDPOINT_SAVE_PRODUCTS,
-      body
-    );
-  }
-  getProducts(id: number = 0): Observable<any> {
-    return this.httpService.get(
-      this.baseUrl + Strings.API_ENDPOINT_GET_PRODUCTS + id
-    );
-  }
-  getProductById(id: number = 0): Observable<any> {
-    return this.httpService.get(
-      this.baseUrl + Strings.API_ENDPOINT_GET_PRODUCT_BY_ID + id
-    );
-  }
-  updateProduct(body: any, id: number): Observable<any> {
-    return this.httpService.put(
-      this.baseUrl + Strings.API_ENDPOINT_UPDATE_PRODUCTS + id,
-      body
-    );
-  }
-  deleteProduct(id: number): Observable<any> {
-    return this.httpService.delete(
-      this.baseUrl + Strings.API_ENDPOINT_DELETE_PRODUCT + id
-    );
-  }
-
-  cancelOrder(orderId: number): Observable<any> {
-    return this.httpService.put(
-      this.baseUrl + Strings.API_ENDPOINT_CANCEL_ORDER,
-      { id: orderId }
-    );
-  }
-
-  addFavourites(body: any): Observable<any> {
-    return this.httpService.post(
-      this.baseUrl + Strings.API_ENDPOINT_FAVOURITE_PRODUCT,
-      body
-    );
-  }
+ 
 }
--- Frontend/src/app/login/login-routing.module.ts
+++ Frontend/src/app/login/login-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { ResetPasswordComponent } from './reset-password/reset-password.component';
-import { OtpVerificationComponent } from './otp-verification/otp-verification.component';
+
 
 const routes: Routes = [
   {
@@ -20,18 +18,7 @@ const routes: Routes = [
     path: 'sign-up',
     component: SignUpComponent,
   },
-  {
-    path: 'forgot-password',
-    component: ForgotPasswordComponent,
-  },
-  {
-    path: 'reset-password/:resetID',
-    component: ResetPasswordComponent,
-  },
-  {
-    path: 'otp-verification',
-    component: OtpVerificationComponent,
-  }
+
 ];
 
 @NgModule({
